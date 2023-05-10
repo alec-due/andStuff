8a6da55710614fcef6d1f117eaa5b076c0b4e2fb
alec-due <133129963+alec-due@users.noreply.github.com>
2023-05-10 10:43:47 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let clicks = 0;
+
+function changeButtonText() {
+        document.querySelector('button').innerHTML = 'You clicked me!';
+        document.querySelector('p').innerHTML = 'arg';
+      }
